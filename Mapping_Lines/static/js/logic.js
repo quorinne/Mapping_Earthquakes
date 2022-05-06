e660ce60d39445c1af76e65fe4a0f3ab3c51f5fd
Brittanie <brittaniejones@ymail.com>
2022-05-06 15:41:21 -0700

    MMP commit 1

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -12,13 +12,27 @@ let map = L.map("mapid", {
     zoom: 4
   });
 
-//  Add a marker to the map for Los Angeles, California.
-L.circleMarker([34.0522, -118.2437], {
-  radius: 300,
-  color: "black",
-  fillColor: '#ffffa1'
-}).addTo(map);
+ //Add a marker to the map for Los Angeles, California.
+// L.circleMarker([34.0522, -118.2437], {
+//   radius: 300,
+//   color: "black",
+//   fillColor: '#ffffa1'
+// }).addTo(map);
 
+
+
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+  console.log(city)
+  L.circleMarker(city.location, {
+    radius: city.population/100000
+  })
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+.addTo(map);
+});
 
   // We create the tile layer that will be the background of our map.
 // We create the tile layer that will be the background of our map.
@@ -30,5 +44,7 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+
 
 
